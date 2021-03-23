93e1a890c4aedbf414afa4c31491596b6c301422
SKY-20201024CGR\Administrator <seayfhzl@gamil.com>
2021-03-23 19:09:56 +0800

    修改登录token方法

--- common/bms_login.py
+++ common/bms_login.py
@@ -1,20 +1,33 @@
-import requests
+"""
+author： mask
+filename: bms_login.py
+datetime： 2021/3/19 0:25
+ide： PyCharm
+"""
+from common.base import get_response
+from test_data.read_data import get_test_data
+from common.logger import Log
+import json
 
 
-def get_token():
-    url = "http://192.168.0.201:8041/auth/oauth/token"
+class getBmsToken(object):
+    def __init__(self):
+        self.test_data = get_test_data('app_test_data.xlsx', 'login', 1)
+        self.log = Log()
+        self.url = self.test_data['url']
+        self.method = self.test_data['method']
+        self.payload = self.test_data['data']
+        self.header = json.loads(self.test_data['header'])
 
-    payload='username=miya&password=123456&code=1111&grant_type=password&scope=server'
-    headers = {
-      'Authorization': 'Basic cGlnOnBpZw==',
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-
-    response = requests.request("POST", url, headers=headers, data=payload).json()
-    access_token = response['token_type'] + ' ' + response['access_token']
-    return access_token
-
-
-if __name__ == '__main__':
-    res = get_token()
-    print(res)
+    def get_token(self):
+        """
+        获取bms登录token
+        :return:token
+        """
+        try:
+            resp = get_response(self.url, self.method,
+                                data=self.payload, headers=self.header)
+            token = resp.json()['token_type'] + ' ' + resp.json()['access_token']
+            return token
+        except Exception as e:
+            self.log.error('获取token出现异常:{}，请检查登录接口请求'.format(str(e)))
